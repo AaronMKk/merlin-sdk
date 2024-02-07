00ba9502606bcb082338ca338dc17d505e5a2c2c
lcr <pkwarcraft@gmail.com>
2024-02-07 14:18:52 +0800

    add user verify token api (#4)
    Signed-off-by: Li Chaoran <pkwarcraft@gmail.com>

---
+++ user/api/api.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+
+	"github.com/openmerlin/merlin-sdk/httpclient"
+	"github.com/openmerlin/merlin-sdk/user"
+)
+
+var (
+	urlToVerifyToken = "/v1/user/token/verify"
+)
+
+func VerifyToken(param *user.RequestToVerifyToken) (code string, err error) {
+	if param == nil || param.Token == "" {
+		code = "invalid param"
+		err = fmt.Errorf("invalid param")
+		return
+	}
+
+	v, err := httpclient.JsonMarshal(param)
+	if err != nil {
+		return
+	}
+
+	req, err := http.NewRequest(http.MethodPost, httpclient.Endpoint(urlToVerifyToken), bytes.NewBuffer(v))
+	if err != nil {
+		return
+	}
+
+	_, code, err = httpclient.Default.ForwardTo(req, nil)
+
+	return
+}
---
+++ user/request.go
@@ -0,0 +1,5 @@
+package user
+
+type RequestToVerifyToken struct {
+	Token string `json:"token"`
+}
